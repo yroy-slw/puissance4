75a4597490045a9517a5c6b1bd041a01503b1b1d
Yann Roy <yann@yannroy.com>
2021-01-11 11:33:35 +0100

    feat(Connect4Game): Added new const labels

---
+++ src/app/common/functionalRules.ts
@@ -0,0 +1,15 @@
+const functionalRules = {
+    APP_TITLE: 'Puissance 4',
+    PLAYERS: {
+        PLAYER1: 'Joueur 1',
+        PLAYER2: 'Joueur 2',
+    },
+    GAME_STATES: {
+        START_LABEL: 'commence.',
+        TURN_LABEL: 'Au tour de ',
+        WIN_LABEL: ' gagne !',
+        NULL_LABEL: 'Egalité !',
+    }
+};
+
+export default functionalRules;
